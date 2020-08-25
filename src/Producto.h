e3b8e96845905fd39925e6f40b9577272ca2cff2
fabio0610 <fabio0610@hotmail.com>
2020-08-25 17:33:27 -0600

    pequeños cambios

--- src/Producto.h
+++ src/Producto.h
@@ -4,9 +4,16 @@
 
 #ifndef MY_PROJECT_NAME_PRODUCTO_H
 #define MY_PROJECT_NAME_PRODUCTO_H
-
+#include <iostream>
+#include <sstream>
+using namespace std;
 
 class Producto {
+private:
+    string nombre;
+    float precio;
+    int cantidad;
+public:
 
 };
 
