fe515ce2412c94ccd0d8ae678b33aa84783622c5
wems99 <allen.blanco.contreras@est.una.ac.cr>
2020-09-08 11:48:57 -0600

    clase maquina implementada pero no desarrollada

--- src/Maquina.cpp
+++ src/Maquina.cpp
@@ -3,3 +3,86 @@
 //
 
 #include "Maquina.h"
+
+Maquina::Maquina(int identificador, const string &nombre, Producto *producto, const vector<Producto> &productoList,
+                 MonederoElectronico *monederoElectronico) : identificador(identificador), nombre(nombre),
+                                                             producto(producto), productoList(productoList),
+                                                             monederoElectronico(monederoElectronico) {}
+
+Maquina::~Maquina() {
+
+}
+
+int Maquina::getIdentificador() {
+
+
+
+}
+
+string Maquina::getNombre() {
+
+
+
+}
+
+void Maquina::setNombre() {
+
+
+
+}
+
+string Maquina::toString() {
+    stringstream s;
+
+
+    return s.str();
+}
+
+void Maquina::insert(Producto *producto) {
+
+
+
+}
+
+void Maquina::addProvisions(string idProducto, int cantidad) {
+
+
+}
+
+void Maquina::decreaseProvisions(string idProducto, int cantidad) {
+
+
+
+}
+
+void Maquina::eliminar(string id) {
+
+
+
+}
+
+Producto Maquina::consultar(string id) {
+
+
+
+}
+
+void Maquina::addMoney() {
+
+
+
+}
+
+void Maquina::drawOutMoney() {
+
+
+
+}
+
+string Maquina::realizarCompra(string id, int cantidad, int montoPagar) {
+
+
+
+}
+
+Maquina::Maquina() {}
--- src/Maquina.h
+++ src/Maquina.h
@@ -4,10 +4,47 @@
 
 #ifndef MY_PROJECT_NAME_MAQUINA_H
 #define MY_PROJECT_NAME_MAQUINA_H
+#include <vector>
+#include "IMaquinaAdministrradora.h"
+#include "IMaquinaVendedora.h"
+#include "MonederoElectronico.h"
 
 
-class Maquina {
+using namespace std;
+class Maquina : public  IMaquinaAdministrradora, IMaquinaVendedora{
 
+private:
+
+    int identificador;
+    string nombre;
+    Producto* producto;
+    vector<Producto> productoList;
+    MonederoElectronico* monederoElectronico;
+
+public:
+
+    Maquina(int identificador, const string &nombre, Producto *producto, const vector<Producto> &productoList,
+            MonederoElectronico *monederoElectronico);
+
+    Maquina();
+
+    //override of IMaquinaAdministradora
+    int getIdentificador() override;
+    string getNombre() override;
+    void setNombre() override;
+    string toString() override;
+    void insert(Producto* producto)  override;
+    void addProvisions(string idProducto, int cantidad) override;
+    void decreaseProvisions(string idProducto, int cantidad) override;
+    void eliminar(string id) override;
+    Producto consultar(string id) override;
+    void addMoney() override;
+    void drawOutMoney() override;
+
+    //override of IMaquinaVendedora
+    string realizarCompra(string id, int cantidad, int montoPagar) override;
+
+    virtual ~Maquina();
 };
 
 
