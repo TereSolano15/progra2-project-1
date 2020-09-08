a944a5568664d58e83d460bd71ca6aa4a9f9d72e
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-09-07 21:09:00 -0600

    clase monedero

--- src/MonederoElectronico.cpp
+++ src/MonederoElectronico.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "MonederoElectronico.h"
+MonederoElectronico::MonederoElectronico() {}
+
+MonederoElectronico::MonederoElectronico(int dinero) : dinero(dinero) {}
+
+MonederoElectronico::~MonederoElectronico() {
+
+}
+
+int MonederoElectronico::getDinero() const {
+    return dinero;
+}
+
+void MonederoElectronico::setDinero(int dinero) {
+    MonederoElectronico::dinero = dinero;
+}
+string MonederoElectronico::desgloceVuelto(string &) {
+
+}
+
+//El monedero electrónico se encarga de manejar
+//el dinero de la máquina. Dentro de esta clase
+//existe un método que se encarga de calcular
+//el vuelto a ser entregado al usuario cuando
+//este cancela (string desgloceVuelto()) este
+//método debe seguir el algoritmo voraz (greedy algorithm)
+//para devolver la menor cantidad de monedas cuando se
+//calcula el vuelto. Este método devuelve en texto el detalle completo del desglose.
+
+Los productos tienen precios cerrados con valores definidos entre 200, 300, 700. El cliente normalmente paga con billetes de 2000 o 5000. Por lo que la máquina debe calcular el desgloce en monedas de 100, monedas de 500, billetes de 1000 y billetes de 2000 cuál es la mejor forma de entregar el vuelto al cliente, este detalle es lo que debe devolver el método.
--- src/MonederoElectronico.h
+++ src/MonederoElectronico.h
@@ -4,9 +4,27 @@
 
 #ifndef MY_PROJECT_NAME_MONEDEROELECTRONICO_H
 #define MY_PROJECT_NAME_MONEDEROELECTRONICO_H
-
+using namespace std;
+#include <sstream>
+#include <iostream>
 
 class MonederoElectronico {
+public:
+    MonederoElectronico(int dinero);
+
+    MonederoElectronico();
+
+    int getDinero() const;
+
+    void setDinero(int dinero);
+
+    virtual ~MonederoElectronico();
+
+    virtual string toString();
+
+    string desgloceVuelto(string& );
+private:
+    int dinero;
 
 };
 
