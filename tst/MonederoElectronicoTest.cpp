0008504d1d9673d76173771aeda005a56e02a1c6
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-09-10 21:12:46 -0600

    MonederoElectronicoTest

---
+++ tst/MonederoElectronicoTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Tere Solano on 10/9/2020.
+//
+#include "MonederoElectronico.h"
+#include "gtest/gtest.h"
+
+
+//TEST(DigitalGameTestSuite, CalculatePriceWithTax){
+//DigitalGame digitalGame("FIFA 20 Standard Edition", 24.39, 0.13, "C:\\fifa20.game");
+
+//EXPECT_DOUBLE_EQ(digitalGame.calculatePriceWithTax(), 23.426595);
+//}
--- tst/main.cpp
+++ tst/main.cpp
@@ -2,6 +2,12 @@
 // Created by Maikol Guzman on 8/26/20.
 //
 
-int main() {
-    return 0;
+#include "gtest/gtest.h"
+#include <iostream>
+
+int main(int argc, char **argv) {
+   std:: cout << "Hello, Universidad Nacional!" << std::endl;
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
