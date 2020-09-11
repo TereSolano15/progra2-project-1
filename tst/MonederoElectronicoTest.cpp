6ed64f3e7ce9bb0ecadfbd299ef2caf383dc3c57
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-09-10 21:15:01 -0600

    MonederoElectronicoTest

--- tst/MonederoElectronicoTest.cpp
+++ tst/MonederoElectronicoTest.cpp
@@ -4,9 +4,3 @@
 #include "MonederoElectronico.h"
 #include "gtest/gtest.h"
 
-
-//TEST(DigitalGameTestSuite, CalculatePriceWithTax){
-//DigitalGame digitalGame("FIFA 20 Standard Edition", 24.39, 0.13, "C:\\fifa20.game");
-
-//EXPECT_DOUBLE_EQ(digitalGame.calculatePriceWithTax(), 23.426595);
-//}
