971bb50df41857e5851a25571a940e1dbb9ebb25
wems99 <allen.blanco.contreras@est.una.ac.cr>
2020-09-13 13:32:13 -0600

    primeros metodos de la clase iadmin desarrollados, los hize a sin ninguna guia, no hay forma de asegurar que estos sirvan

--- src/IMaquinaAdministrradora.h
+++ src/IMaquinaAdministrradora.h
@@ -17,9 +17,9 @@ public:
 
     virtual int getIdentificador() = 0;
     virtual string getNombre() = 0;
-    virtual void setNombre() = 0;
+    virtual void setNombre(string nombre) = 0;
     virtual string toString() = 0;
-    virtual void insert(Producto* producto) = 0;
+    virtual void insert(Producto producto) = 0;
     virtual void addProvisions(string idProducto, int cantidad) = 0;
     virtual void decreaseProvisions(string idProducto, int cantidad) = 0;
     virtual void eliminar(string id) = 0;
