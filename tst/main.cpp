4904b7f182986b5fbecd3f32cbb4211da6be2445
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-09-15 10:46:47 -0600

    Test arreglos

--- tst/main.cpp
+++ tst/main.cpp
@@ -2,12 +2,10 @@
 // Created by Maikol Guzman on 8/26/20.
 //
 
-#include "gtest/gtest.h"
+//#include "gtest/gtest.h"
 #include <iostream>
 
 int main(int argc, char **argv) {
    std:: cout << "Hello, Universidad Nacional!" << std::endl;
 
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
