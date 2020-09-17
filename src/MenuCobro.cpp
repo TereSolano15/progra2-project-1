5dc22226d56daaac002cb446dfee3ac5ea99fd17
fabio0610 <fabio0610@hotmail.com>
2020-09-16 23:38:26 -0600

    se modifica poco "MenuCobro"

--- src/MenuCobro.cpp
+++ src/MenuCobro.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "MenuCobro.h"
+
+MenuCobro::MenuCobro() {}
+
+MenuCobro::MenuCobro(MonederoElectronico *monedero, Maquina *maquina) : monedero(monedero), maquina(maquina) {}
+
+void MenuCobro::mostrar2() {
+    int opcion = -1;
+    do {
+        system("cls");
+        opcion = menuCobro();
+        switch (opcion) {
+            case 1:
+                //met11();
+                break;
+            case 2:
+                // met12();
+                system("pause");
+                break;
+            case 3:
+                break;
+            default:
+                cout << "Opcion invalida" << endl;
+        }
+    } while (opcion < 3 && opcion > 0);
+}
+
+int MenuCobro::menuCobro() {
+    int opcion;
+    cout << "Esta en el menu de Cobro, elija una opcion:" << endl;
+    cout << "1) Mostrar Producto" << endl;
+    cout << "2) Seleccionar Producto" << endl;
+    cout << "3) Regresar al menu principal" << endl;
+    cin >> opcion;
+    return opcion;
+}
--- src/MenuCobro.h
+++ src/MenuCobro.h
@@ -4,10 +4,25 @@
 
 #ifndef MY_PROJECT_NAME_MENUCOBRO_H
 #define MY_PROJECT_NAME_MENUCOBRO_H
-
+#include "Maquina.h"
+#include "MonederoElectronico.h"
+#include "ProductoPerecedero.h"
+#include "ProductoNoPerecedero.h"
+#include <iostream>
+#include <sstream>
 
 class MenuCobro {
+private:
+    MonederoElectronico* monedero;
+    Maquina* maquina;
+public:
+    MenuCobro();
 
+    MenuCobro(MonederoElectronico *monedero, Maquina *maquina);
+
+    void mostrar2();
+
+    int menuCobro();
 };
 
 
